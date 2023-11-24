22953c7753d4e67ccf723d60325fd5d93d439c6c
Mohammad Karami <1380mkkarami@gmail.com>
2023-11-24 13:03:36 +0330

    Initiate course app

---
+++ course/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from user.models import Professor, Student
+
+
+class Course(models.Model):
+    teacher = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    subject = models.TextField()
+
+class Attendee(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
