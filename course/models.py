667fa2b82baea0c3711ca2defc11bba273de4060
Mohammad Karami <1380mkkarami@gmail.com>
2024-01-25 12:40:58 +0330

    Add course session models

--- course/models.py
+++ course/models.py
@@ -2,13 +2,36 @@ from django.db import models
 
 from user.models import Professor, Student
 
+
 class CourseSubject(models.Model):
-    prerequisites = models.ManyToManyField('self')
+    prerequisites = models.ManyToManyField("self")
     subject = models.TextField(unique=True)
+
 
 class Course(models.Model):
     teacher = models.ForeignKey(Professor, on_delete=models.CASCADE)
     subject = models.ForeignKey(CourseSubject, on_delete=models.PROTECT)
+
+
+class CourseSession(models.Model):
+    class Weekday(models.IntegerChoices):
+        MONDAY = 1
+        TUESDAY = 2
+        WEDNESDAY = 3
+        THURSDAY = 4
+        FRIDAY = 5
+        SATURDAY = 6
+        SUNDAY = 7
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    holds_even_weeks = models.BooleanField(default=True)
+    holds_odd_weeks = models.BooleanField(default=True)
+    weekday = models.IntegerField(choices=Weekday)
+    time_slot = models.IntegerChoices(
+        "TimeSlot",
+        "8.AM 10.AM 12.AM 2.PM 4.PM 6.PM",
+    )
 
 
 class Enrolment(models.Model):
